796f6c695770220a3fcdb2ac7b837524481b9c4f
David Weidman E Hour <david.weidman@usma.edu>
2016-02-26 10:14:18 -0500

    Added files via upload

---
+++ geolocWorkspace.py
@@ -0,0 +1,15 @@
+constants gpspair 1,2,3
+
+def pdoa(self):
+	#receive message
+	#unpack data
+	#check timestamp
+	#create 3 queues to hold data
+	#(pwoer and gps)
+	for x <- xrange:
+		for y <- yrange:
+			#will have timestamp data
+			d1= distance from circle 1
+			d2= distance from circle 2
+			d3= distance from circle 3
+			array(x)(y)= intensity(d1)*intensity(d2)*intensity(d3)
